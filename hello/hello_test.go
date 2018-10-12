01df508c87b6df9c6e256fe5657a30db685b9f50
Mirella Batista <mbatista@icontrol.com>
2018-10-12 11:32:05 -0500

    Week one homework - hellow, world

---
+++ hello/hello_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T){
+
+    assertCorrectMessage := func(t *testing.T, got, want string){
+        t.Helper()
+        if got != want {
+            t.Errorf("got '%s' want '%s'", got, want)
+        }
+    }
+
+    t.Run("saying hello to people", func(t *testing.T){
+        got := Hello("Mirella")
+        want := "Hello, Mirella"
+
+        assertCorrectMessage(t, got, want)
+    })
+    
+    t.Run("say 'Hello, World' when an empty strin is supplied", func(t *testing.T){
+        got := Hello("")
+        want := "Hello, World"
+
+        assertCorrectMessage(t, got, want)
+    })
+    
+}
