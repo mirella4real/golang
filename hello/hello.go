56efb4b1732abcc62384b100b533e9988b38c183
Mirella Batista <mbatista@icontrol.com>
2018-10-12 11:53:15 -0500

    Adding language param

--- hello/hello.go
+++ hello/hello.go
@@ -4,13 +4,17 @@ import "fmt"
 
 const helloPrefix =  "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
     if name == ""{
         name = "World"
+    }
+
+    if language == "Spanish"{
+        return "Hola, " + name
     }
     return helloPrefix + name
 }
 
 func main(){
-    fmt.Println(Hello("Mirella"))
+    fmt.Println(Hello("Mirella", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,15 +12,22 @@ func TestHello(t *testing.T){
     }
 
     t.Run("saying hello to people", func(t *testing.T){
-        got := Hello("Mirella")
+        got := Hello("Mirella", "")
         want := "Hello, Mirella"
 
         assertCorrectMessage(t, got, want)
     })
     
     t.Run("say 'Hello, World' when an empty strin is supplied", func(t *testing.T){
-        got := Hello("")
+        got := Hello("", "")
         want := "Hello, World"
+
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in Spanish", func(t *testing.T){
+        got := Hello("Mirella", "Spanish")
+        want := "Hola, Mirella"
 
         assertCorrectMessage(t, got, want)
     })
